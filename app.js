86e79332eccb1cf5032370a160a6afd57b06534d
Marcos William <mwsb.dev@gmail.com>
2025-01-30 00:45:09 -0300

    Func adicionar array  e limpar ok

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let arrayOfFriends = [];
+
+function adicionarAmigo() {
+    let temp = document.querySelector("input");
+    if(temp.value == 0) {
+        alert("Nome inválido!");
+        limparCampo();
+    } else {
+        arrayOfFriends.push(temp.value);
+        console.log("Teste");
+        console.log(arrayOfFriends);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    limpar = document.querySelector("input");
+    limpar.value = "";
+}
