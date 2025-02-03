f8eff937facd84309ff1f40efbc816ca169eee12
Marcos William <mwsb.dev@gmail.com>
2025-02-03 14:55:21 -0300

    Todas as funções funcionando

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let arrayOfFriends = [];
+let amigos = [];
 
 function adicionarAmigo() {
     let temp = document.querySelector("input");
-    if(temp.value == 0) {
-        alert("Nome inválido!");
+    if(temp.value == "") {
+        alert("Por favor, insira um nome.");
         limparCampo();
-    } else {
-        arrayOfFriends.push(temp.value);
+    } else if((amigos.includes(temp.value))){
+        alert("Nome já incluso!");
+        limparCampo();
+    } else{
+        amigos.push(temp.value);
         console.log("Teste");
-        console.log(arrayOfFriends);
+        console.log(amigos);
         limparCampo();
+        listaAmigos();
     }
 }
 
 function limparCampo() {
-    limpar = document.querySelector("input");
+    let limpar = document.querySelector("input");
     limpar.value = "";
+}
+
+function listaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for( let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if(amigos.length <= 1){
+        alert("Faça a lista primeiro.");
+        console.log("teste");
+    } else {
+        let max = amigos.length;
+        let random = Math.floor(Math.random()*max);
+
+        alert(`O amigo sorteado foi ${amigos[random]}`)
+    }
 }
